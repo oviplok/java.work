ecb2f5ed5e03bdc7b534a5dd14ae14e327357ab6
oviplok <90204538+oviplok@users.noreply.github.com>
2021-12-18 14:54:51 +0300

    Update lab12.java

--- lab12/lab12.java
+++ lab12/lab12.java
@@ -1,22 +1,28 @@
 package lab12;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class lab12 {
+
     public static boolean checkEmail(String s){
         Matcher m = Pattern.compile(
-                "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?!-)(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$"
-        ).matcher(s);
+                "[a-zA-Z0-9]+[a-zA-Z0-9!#$%&'*+\\-/=?^_`{|}~.]*@[a-zA-Z0-9\\-_.]+\\.[a-zA-Z]+"
+            
         return m.find() && m.group().equals(s);
     }
 
+
     public static void main(String[] args) {
-        System.out.println("nikita.eg0110@mail.ru: "+checkEmail("nikita.eg0110@mail.ru"));
-        System.out.println("niita@gmail.com: "+checkEmail("niita@gmail.com"));
-        System.out.println(".@mail.ru:"+checkEmail(".@mail.ru"));
-        System.out.println("nikich@.mail:"+checkEmail("nikich@.mail"));
-        System.out.println("eg@g@mail.ru: "+checkEmail("eg@g@mail.ru"));
+
+        //ex6(Email)
+        System.out.println();
+        System.out.println(checkEmail("sagrachov@gmail.com"));
+        System.out.println(checkEmail(".sag rachov@gmail.com"));
+        System.out.println(checkEmail("sagrachov.@.gmail.com"));
+        System.out.println(checkEmail("sagrachov@gmail"));
+        System.out.println(checkEmail("sagrachov@g@mail.ru"));
+
     }
 }
